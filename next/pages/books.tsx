3dc539f5286255e25ec8164259648de90f27ce5c
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2021-11-23 22:15:05 +0900

    install SWR but not working

--- next/pages/books.tsx
+++ next/pages/books.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+import useSWR from "swr"
 import { Book } from "../types/book"
 import BookListContextProvider from "../context/bookList"
 import ListTemp from "../components/templates/List/main"
@@ -7,11 +9,26 @@ type Props = {
   staticfilterdBooks: Book[];
 }
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}/books`;
+
 // 引数にgetStaticPropsで得られたpropsが入る
 const Books = ({ staticfilterdBooks }: Props) => {
+  const { data: books, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticfilterdBooks, //初期データセット
+    refreshInterval: 3000 //更新のタイミングを設定
+  });
+
+  useEffect(() => {
+    console.log('swr effect');
+    fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/books`).then((res) => console.log(res.json())) //ここで最新データは取れているが
+    mutate(apiUrl);
+    console.log(books); // booksに最新に入れたデータが登録されていない、これが原因
+  }, []);
 
   return (
-    <BookListContextProvider books={staticfilterdBooks}>
+    // <BookListContextProvider books={staticfilterdBooks}>
+    <BookListContextProvider books={books}>
       <ListTemp />
     </BookListContextProvider>
   )
