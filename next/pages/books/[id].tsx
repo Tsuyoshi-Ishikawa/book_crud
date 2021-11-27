e167d7c0dc172f4ad65f032b8937480e70252981
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2021-11-27 22:44:48 +0900

    complete fallback

--- next/pages/books/[id].tsx
+++ next/pages/books/[id].tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { Book } from "../../types/book";
 import { getAllBookIds, getBookData } from "../../lib/book";
 import DetailTemp from "../../components/templates/Detail/main"
@@ -7,6 +8,12 @@ type Props = {
 }
 
 const BookDetail = ({ book }: Props) => {
+  const router = useRouter();
+
+  // まだSSGしていないページをSSGする時の待ち時間で表示
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -24,7 +31,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
     // fallbackがtrueの場合は SSGした時にcannot read property 渡している値 of undefinedになることがある
 
     // 原因
@@ -36,15 +43,15 @@ export async function getStaticPaths() {
     // if (router.isFallback || !book) {
     //   return <div>Loading...</div>;
     // }
-    // だが/books/{存在しないID}で接続するとサーバーエラーになる。
-    // getStaticPropsで返すprops.bookが空になり、SSGができないからと思われる。
-    // つまり、もしfallback: trueにするならば、props.bookが空データならば、
-    // 404ページを返すようにする必要がある。その分岐をcomponent側に実装する。
 
-    // fallback: trueにするのは
-    // 詳細ページがものすごく多い+ 変更する頻度が高い場合などに使用するのが良さそう
+    // だが/books/{存在しないID}で接続するとサーバーエラーになる。
+    // getStaticPropsのgetBookData(params.id)が空になり、SSGができないからと思われる。
+    // つまり、もしfallback: trueにするならば、getBookData(params.id)が空データの場合に、
+    // 404ページを返すようにする必要がある→notFound: trueで対応
+
+    // fallback: trueにするユースケースは
+    // 詳細ページがものすごく多い+ 変更する頻度が高い場合などに使用す場合
     // https://nextjs.org/docs/basic-features/data-fetching#when-is-fallback-true-useful
-    // 基本的にはfallback: falseにするのが良いと思われる,
   }
 }
 
@@ -52,10 +59,24 @@ type Params = {
   id: string;
 }
 
-// paramsにpathに設定されている変数が入っている
+// 下記の場合にgetStaticPropsが走る
+// ・事前にSSGする時
+// ・SSGされていないページへのアクセスがあった時
+// ・ISRの時
 export async function getStaticProps({ params }: { params: Params }) {
-  console.log(params.id)
+  // paramsにpathに設定されている変数が入っている
   const book = await getBookData(params.id);
+
+  // APiから空のデータが帰る場合は404ページを返す、ちゃんとstatus codeも404なので良い。
+  // https://nextjs.org/blog/next-10#notfound-support
+  // 開発環境で動かすとUnhandled Runtime Error Error: Failed to load script: /_next/static/chunks/pages/404.jsというエラーが出るが、
+  // 本番(yarn build & yarn start)では表示されないので、一旦OKとする
+  // https://stackoverflow.com/questions/68939360/next-js-unhandled-runtime-error-failed-to-load-script-next-static-chunks
+  if (book.id === "") {
+    return {
+      notFound: true
+    };
+  };
 
   return {
     props: { book },
